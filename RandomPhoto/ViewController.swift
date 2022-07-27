d7690f805a56af0d2295df81726d9c995b4cd4c7
Andrew Gravino <aggravino@cpp.edu>
2022-07-27 16:39:03 -0700

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Andrew Gravino on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
